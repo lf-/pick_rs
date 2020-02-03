2eec4e3f11a90497f6479abb991246fb8f72badc
lf- <lf-@users.noreply.github.com>
2020-02-03 00:35:20 -0800

    switch to sdl2 because its window centering is better

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use glutin_window::GlutinWindow;
-use piston::window::{AdvancedWindow, Window};
-use piston_window::{clear, PistonWindow, Position, WindowSettings};
+use piston::window::Window;
+use piston_window::{clear, EventLoop, PistonWindow, WindowSettings};
+use sdl2::video::WindowPos;
+use sdl2_window::Sdl2Window;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::time::Instant;
@@ -13,27 +14,22 @@ use app::App;
 
 fn main() {
     let now = Instant::now();
-    let mut window: PistonWindow<GlutinWindow> =
+    let mut window: PistonWindow<Sdl2Window> =
         WindowSettings::new("Hello Piston!", layout::WINDOW_DIMS)
             .exit_on_esc(true)
             .decorated(false)
             .build()
             .unwrap();
+
+    window.set_max_fps(60);
+    window.set_ups(60);
     println!("build window {}", now.elapsed().as_millis());
     let now = Instant::now();
-    let win = window.window.ctx.window();
-    let monitor_id = win.get_primary_monitor();
-    let msize = monitor_id.get_dimensions();
-    let mpos = monitor_id.get_position();
-    println!("get window dims {}", now.elapsed().as_millis());
 
-    let center = Position {
-        x: (mpos.x + msize.width / 2. - window.size().width / 2.) as i32,
-        y: (mpos.y + msize.height / 2. - window.size().height / 2.) as i32,
-    };
-
-    let now = Instant::now();
-    window.set_position(center);
+    window
+        .window
+        .window
+        .set_position(WindowPos::Centered, WindowPos::Centered);
     println!("center window {}", now.elapsed().as_millis());
 
     let mut gcache = window.load_font("FiraSans-Regular.ttf").unwrap();
